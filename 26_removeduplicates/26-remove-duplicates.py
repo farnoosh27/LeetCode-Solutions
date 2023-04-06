6ee5368770fa395fb67aacf279c638c6a73d27f7
farnoosh azour <72529423+farnoosh27@users.noreply.github.com>
2023-04-05 23:36:47 -0400

    Add files via upload

---
+++ 26_removeduplicates/26-remove-duplicates.py
@@ -0,0 +1,19 @@
+
+def removeDuplicates(nums):
+    # we need to us two pointers:
+        # l for left --> tell us the next unique value
+        # l also tells us how many unique values
+        # r for right --> scanning throught the array
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[l] = nums[r]
+                l += 1
+        return(l)
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(removeDuplicates(nums))
+
+
+        
+    
