df452e7a87acdce350bb5705d69d91d3944c6d15
farnoosh azour <72529423+farnoosh27@users.noreply.github.com>
2023-04-08 19:55:46 -0400

    Update 169_majorityelement_solution2.py

--- 169_majorityelement/169_majorityelement_solution2.py
+++ 169_majorityelement/169_majorityelement_solution2.py
@@ -12,6 +12,11 @@ def majorityElement( nums):
         else:
             count -= 1
     return res
+# The Boyer-Moore majority vote algorithm has a 
+# time complexity of O(n) and a space complexity of O(1).
+
+
+
 
 
      
