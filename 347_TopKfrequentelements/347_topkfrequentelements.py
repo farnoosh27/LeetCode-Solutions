1d384e161dccae63a819a50df2dec52d087478e7
farnoosh azour <72529423+farnoosh27@users.noreply.github.com>
2023-04-08 22:58:55 -0400

    Update 347_topkfrequentelements.py

--- 347_TopKfrequentelements/347_topkfrequentelements.py
+++ 347_TopKfrequentelements/347_topkfrequentelements.py
@@ -3,20 +3,24 @@ def topkfrequentelements(nums, k):
     # this is the bucket sort solution
     # but for this bucket sort we are going through the array 
     # the time complexity of a bucket sort is O(n)
-    count = {}
-    freq = [[] for i in range(len(nums)+1)]
-    for n in nums: 
-        count[n] = 1 + count.get(n,0)
+  count = {}
+    for i in range(len(nums)):
+        if nums[i] in count:
+            count[nums[i]] += 1
+        else:
+            count[nums[i]] = 1
 
-    for n, c in count.items():
-        freq[c].append(n)
+    # create a list of lists to store numbers with the same frequency
+    freq = [[] for _ in range(len(nums) + 1)]
+    for num, count in count.items():
+        freq[count].append(num)
 
+    # iterate through the frequency list from the end and add the elements to the result list
     res = []
-    for i in range(len(freq)-1, 0, -1):
-        for n in freq[i]:
-            res.append(n)
-            if len(res) == k:
-                return res
+    for i in range(len(freq) - 1, 0, -1):
+        res.extend(freq[i])
+        if len(res) == k:
+            return res
 nums = [0,0,1,1,1,3,3,3,3,2,2,3,3,4]
 k = 2
 print(topkfrequentelements(nums, k))
