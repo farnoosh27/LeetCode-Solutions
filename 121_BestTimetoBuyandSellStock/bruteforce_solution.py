67ccb88bbd0dda2a0ea147b08ac6f8ba0b7f55a1
farnoosh azour <72529423+farnoosh27@users.noreply.github.com>
2023-04-10 17:53:52 -0400

    Add files via upload

---
+++ 121_BestTimetoBuyandSellStock/bruteforce_solution.py
@@ -0,0 +1,13 @@
+
+def maxProfit( prices):
+    ## for brute force solution we can certainly define a max 
+    # assume j is more than i and return the maximum of prices[j]-prices[i]
+    max_profit = 0
+    for i in range(len(prices)):
+        for j in range(i+1,len(prices)):
+            profit = prices[j]-prices[i]
+            max_profit = max(max_profit, profit)
+    return(max_profit)
+
+prices = [7,1,5,3,6,4]
+print(maxProfit(prices))
